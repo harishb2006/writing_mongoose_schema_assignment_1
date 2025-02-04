1c625fb6a033d075b834541278a13074f47d5bed
harishb2006 <imharishba@gmail.com>
2025-02-04 12:35:34 +0530

    updated

--- schema.js
+++ schema.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
     role: {
-        type: String,
+        type:[String],
         enum: ['user', 'admin'],
         default: 'user',
     },
